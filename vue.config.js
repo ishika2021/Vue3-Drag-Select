e628e776569f1b9793d3e726f9868bb818f2a220
Ishika <mishraishika7@gmail.com>
2024-07-05 08:48:30 +0530

    updates vue config with repository name

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/Vue3DragSelect" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/Vue3-Drag-Select" : "/",
 })
